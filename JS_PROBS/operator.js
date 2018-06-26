df11745457aebdf5208db7e4e7f04373ead9b028
vaarigupta <vaarigupta24@gmail.com>
2018-06-27 03:29:24 +0530

    operator file created

---
+++ JS_PROBS/operator.js
@@ -0,0 +1,29 @@
+var a ,b;
+function compare(a,b)
+{
+	if(a>b)
+	{   console.log(a ,"is greater than", b)
+		console.log(true)
+	}
+	else if (a<b) {
+		console.log(a ,"is less than", b)
+		console.log(true)
+	}
+	else if (a==b) {
+		console.log(a ,"is equal to(only value same) ", b)
+		console.log(true)
+	}
+	else if (a===b) {
+		console.log(a ,"is equal equal to (type and value same)", b)
+		console.log(true)
+	}
+	else
+	{   console.log("not an issue")
+		console.log(false)
+	}
+}
+compare(2,3)
+compare(4,3)
+compare(2,2)
+compare(2,"2")
+compare(2,"3")
