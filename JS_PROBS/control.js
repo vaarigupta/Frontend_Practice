33d0354ea16778da15ec6121218acf7155acc6ff
vaarigupta <vaarigupta24@gmail.com>
2018-07-02 17:35:59 +0530

    control file added

---
+++ JS_PROBS/control.js
@@ -0,0 +1,14 @@
+var ham = 10 
+var cheese = 10
+if (ham ===10 && cheese ===10)
+{
+	console.log("sold fully");
+}
+else if (ham === 0 && cheese ===0)
+{
+	console.log("nothing sold");
+}
+else 
+{
+	console.log("partially sold")
+}
