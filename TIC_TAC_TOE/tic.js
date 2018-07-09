a0ae5c2355e719d0c0fb11ca9e5853fe111ab456
vaarigupta <vaarigupta24@gmail.com>
2018-07-09 18:27:20 +0530

    tic tac toe game started

---
+++ TIC_TAC_TOE/tic.js
@@ -0,0 +1,18 @@
+var header = document.querySelector("h1")
+
+function getRandomColor(){
+  var letters = "0123456789ABCDEF";
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random()*16)];
+  }
+  return color
+}
+
+// Simple function for clarity
+function changeHeaderColor(){
+  colorInput = getRandomColor()
+  header.style.color = colorInput
+}
+setInterval("changeHeaderColor()",500);
+
