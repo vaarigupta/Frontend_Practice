9c40f33cf66b0dd7834a0253bf0fa371ef689380
vaarigupta <vaarigupta24@gmail.com>
2018-07-09 22:40:16 +0530

    tic tac toe built

--- TIC_TAC_TOE/tic.js
+++ TIC_TAC_TOE/tic.js
@@ -16,3 +16,39 @@ function changeHeaderColor(){
 }
 setInterval("changeHeaderColor()",500);
 
+
+var restart = document.getElementById("restart");
+var squares = document.querySelectorAll('td');
+
+
+function clear()
+{
+  for(var i = 0 ; i< squares.length ; i++)
+  {
+    squares[i].textContent = "";
+  }
+}
+
+restart.addEventListener('click',clear)
+
+function change()
+{
+  if(this.textContent === '')
+  {
+    this.textContent = 'X';
+  }
+  else if(this.textContent === 'X')
+  {
+     this.textContent = 'O'
+  }
+  else
+  {
+    this.textContent = ''
+  }
+}
+
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click',change)
+}
+
